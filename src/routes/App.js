5a49c333043c3295a078b468c0164a21362a72f7
manu-rt <mrodrigueztorroba@gmail.com>
2021-04-27 19:42:59 +0200

    los path de los componentes en App.js se han cambiado para buscar en la url adecuada

--- src/routes/App.js
+++ src/routes/App.js
@@ -13,9 +13,9 @@ const App = () =>  {
        <Layout>
          <Switch>
            <Route exact path="/curso-react" component={Home} />
-           <Route exact path="/curso-react/login" component={Login} />
-           <Route exact path="/curso-react/register" component={Register} />
-           <Route exact path="/curso-react/player/:id" component={Player} />
+           <Route exact path="/login" component={Login} />
+           <Route exact path="/register" component={Register} />
+           <Route exact path="/player/:id" component={Player} />
            <Route component={NotFound} /> 
          </Switch>  
        </Layout>    
