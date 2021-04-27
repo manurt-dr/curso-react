e658c44ab4106c6e98483c78ead90cc2fa604d1b
manu-rt <mrodrigueztorroba@gmail.com>
2021-04-27 19:02:38 +0200

    los path de los componentes en App.js se han cambiado para buscar en la url adecuada

--- src/routes/App.js
+++ src/routes/App.js
@@ -12,10 +12,10 @@ const App = () =>  {
      <BrowserRouter basename={process.env.PUBLIC_URL}>
        <Layout>
          <Switch>
-           <Route exact path="/" component={Home} />
-           <Route exact path="/login" component={Login} />
-           <Route exact path="/register" component={Register} />
-           <Route exact path="/player/:id" component={Player} />
+           <Route exact path="/curso-react" component={Home} />
+           <Route exact path="/curso-react/login" component={Login} />
+           <Route exact path="/curso-react/register" component={Register} />
+           <Route exact path="/curso-react/player/:id" component={Player} />
            <Route component={NotFound} /> 
          </Switch>  
        </Layout>    
